8787ed66520ab6be19c7f4a3bcc04c20e5741dfa
Van Tuan Dao <v.dao@inmed-gmbh.de>
2023-04-28 14:12:16 +0200

    [+] Add Learning

---
+++ src/main/java/com/waxy/database/entity/Learning.java
@@ -0,0 +1,25 @@
+package com.waxy.database.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Learning {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String videoLink;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+}
