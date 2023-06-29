215157ee7e480b1a98bd8bf05ce2776fdde2d6b0
dtuanv <dtuanlieb@gmail.com>
2023-06-29 22:14:00 +0200

    [+] Add Business Entity

---
+++ src/main/java/com/waxy/database/repository/BusinessRepository.java
@@ -0,0 +1,9 @@
+package com.waxy.database.repository;
+
+import com.waxy.database.entity.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusinessRepository extends JpaRepository<Business, Long> {
+}
