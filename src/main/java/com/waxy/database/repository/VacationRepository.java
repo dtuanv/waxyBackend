5d3b829aa58ca634257bac20ff9e83ba0036e322
Van Tuan Dao <v.dao@inmed-gmbh.de>
2023-06-30 12:07:42 +0200

    [+] Admin confirm and reject VacationRequest. In Vacation list, show only the confirmed Vacations

--- src/main/java/com/waxy/database/repository/VacationRepository.java
+++ src/main/java/com/waxy/database/repository/VacationRepository.java
@@ -2,13 +2,34 @@ package com.waxy.database.repository;
 
 import com.waxy.database.entity.Vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Repository
 public interface VacationRepository extends JpaRepository<Vacation, Long> {
-    @Query(value="SELECT * FROM vacation WHERE business_id = ?1", nativeQuery = true)
+    @Query(value="SELECT * FROM vacation WHERE business_id= ?1 AND is_confirmed = true", nativeQuery = true)
     Set<Vacation> findAllByBusinessId(int businessId);
+
+    @Query(value="SELECT count(*) FROM vacation WHERE is_confirmed = false AND business_id = ?1" , nativeQuery = true)
+    int countVacationIsNotConfirm(int businessId);
+
+    @Query(value="SELECT * FROM vacation WHERE is_confirmed = false AND is_rejected = false AND business_id = ?1" , nativeQuery = true)
+    LinkedHashSet<Vacation> findVacationIsNotConfirm(int businessId);
+
+
+    @Transactional
+    @Modifying
+    @Query(value="UPDATE vacation SET is_rejected = true WHERE id = ?1", nativeQuery = true)
+    void updateVacationColumnIsRejected(long vacationId);
+
+
+    @Transactional
+    @Modifying
+    @Query(value="UPDATE vacation SET is_Confirmed = true WHERE id = ?1", nativeQuery = true)
+    void updateVacationColumnIsConfirmed(long vacationId);
 }
