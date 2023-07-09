9eba662ed4eac008bb1e7d97726135ceb7e1389c
dtuanv <dtuanlieb@gmail.com>
2023-07-09 15:16:34 +0200

    [+] Add Api for Admin

--- src/main/java/com/waxy/database/repository/UserRepository.java
+++ src/main/java/com/waxy/database/repository/UserRepository.java
@@ -10,6 +10,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserDTO, Long> {
 
+    @Query(value = "SELECT COUNT(*) FROM user_info WHERE business_id = ?1", nativeQuery = true)
+    Integer countUserOfBusiness(long businessId);
     @Query("From UserDTO WHERE username=?1")
     Optional<UserDTO> findByUsername(String username);
 
