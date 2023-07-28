a9819180984756ffe96f9c9b5c6abbcad05fcedc
Van Tuan Dao <v.dao@inmed-gmbh.de>
2023-07-28 15:09:37 +0200

    [*]FIX show WishMessage

--- src/main/java/com/waxy/database/repository/WishMessageRepository.java
+++ src/main/java/com/waxy/database/repository/WishMessageRepository.java
@@ -16,6 +16,11 @@ public interface WishMessageRepository extends JpaRepository<WishMessage, Long> 
     @Query(value="SELECT * FROM wish_message WHERE from_user_id = ?1 AND birth_user_id = ?2 AND today LIKE ?3", nativeQuery = true)
     Set<WishMessage> checkWishMessageFromUser(long fromUserId, long birthUserId, String today);
 
-    @Query(value="SELECT * FROM wish_message WHERE today LIKE ?1 AND birth_user_id = ?2 AND message != ''",nativeQuery = true)
-    Set<WishMessage> findWishMessageToMe(String today, long userId);
+    @Query(value="SELECT *\n" +
+            "FROM wish_message\n" +
+            "WHERE\n" +
+            "    EXTRACT(MONTH FROM TO_DATE(today, 'DD-MM-YYYY')) = ?1\n" +
+            "    AND EXTRACT(DAY FROM TO_DATE(today, 'DD-MM-YYYY')) = ?2\n" +
+            "    AND birth_user_id = ?3 AND message != '';",nativeQuery = true)
+    Set<WishMessage> findWishMessageToMe(int month,int day, long userId);
 }
