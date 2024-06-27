a0b33ab871cd60a71170b38a01b7670e1e17e9d9
dtuanv <dtuanlieb@gmail.com>
2024-06-27 23:37:07 +0200

    [+] updateReadContactQuery

--- src/main/java/com/waxy/database/repository/ContactRepository.java
+++ src/main/java/com/waxy/database/repository/ContactRepository.java
@@ -2,8 +2,11 @@ package com.waxy.database.repository;
 
 import com.waxy.database.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+    @Query(value="UPDATE CONTACT SET is_read = ?1 WHERE id = ?2 RETURNING id, message, mobil , create_at, is_read ", nativeQuery = true)
+    Contact updateReadContactQuery(boolean isRead, long id);
 }
