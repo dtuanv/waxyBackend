e4d05c7f51a539d91f97c5bcde28eeb69a306ddb
dtuanv <dtuanlieb@gmail.com>
2024-10-31 22:11:01 +0100

    [TEST] testGetUsernameFromJwtToken, testValidateJwtToken

--- src/test/java/com/waxy/utils/JwtUtilTest.java
+++ src/test/java/com/waxy/utils/JwtUtilTest.java
@@ -1,18 +1,42 @@
 package com.waxy.utils;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.waxy.utils.JwtUtil.generateJwtToken;
+import static com.waxy.utils.JwtUtil.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtUtilTest {
-
+    static  String token;
+    static String username = "user@waxy";
+    @BeforeAll
+    public static void setUp(){
+         token =  generateJwtToken(username);
+    }
 
     @Test
-    public void generateJwtTokenTest(){
+    public void testGenerateJwtToken(){
         String token = generateJwtToken("an");
 
         System.out.println(token);
         assertEquals(token.length()> 25, true);
+    }
+
+    @Test
+    public void testGetUsernameFromJwtToken() {
+        String usernameRes = getUsernameFromJwtToken(token);
+
+        assertEquals(usernameRes,username);
+    }
+
+    @Test
+    public void  testValidateJwtToken(){
+        boolean validTokenResponse = validateJwtToken(token);
+        boolean invalidTokenResponse = validateJwtToken(token+ "in");
+
+        assertEquals(validTokenResponse, true);
+        assertEquals(invalidTokenResponse, false);
+
     }
 }
