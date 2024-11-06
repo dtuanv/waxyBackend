70ab24c113bac7d89c6e439a72bf11a4ba243973
dtuanv <dtuanlieb@gmail.com>
2024-11-07 00:43:48 +0100

    [TEST] @ActiveProfiles("test")

--- src/test/java/com/waxy/service/layout/EssentialLinkServiceTest.java
+++ src/test/java/com/waxy/service/layout/EssentialLinkServiceTest.java
@@ -12,12 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 
-
 @SpringBootTest
+@ActiveProfiles("test")
 class EssentialLinkServiceTest {
     @Autowired
     EssentialLinkRepository essentialLinkRepository;
