b5efc59837bd90e8ea2113776f97ab449161524a
dtuanv <dtuanlieb@gmail.com>
2025-06-07 20:42:12 +0200

    [+] handle exception when sending mail

--- src/main/java/com/waxy/service/EmailService.java
+++ src/main/java/com/waxy/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.waxy.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -8,17 +10,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
     @Autowired
     private JavaMailSender mailSender;
     @Value("${spring.mail.username}") // Injecting the email username
     private String fromAddress;
     public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromAddress); // Ensure the "from" address is set
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromAddress); // Ensure the "from" address is set
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            mailSender.send(message);
+        }catch (Exception e){
+            logger.error("Error sending email to {}: {}", to, e.getMessage(), e);
+        }
+
     }
 }
